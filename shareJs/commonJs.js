dfb98c1d9cba7435d8b2c2b926a308a90f369ab6
Damien Schaller <damienschaller23@gmail.com>
2019-07-24 11:41:30 +0200

    music management by sort

--- shareJs/commonJs.js
+++ shareJs/commonJs.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
 
-    $('#searchForm').on('submit', function(evt) {
+    // Form summit
+    $('form#searchForm').on('submit', function(evt) {
         evt.preventDefault();
 
         const recherche = $('#searchText').val();
+        const filter = $('#filter').val();
 
         // call AJAX
-        const requete = $.ajax({
-            url : 'https://api.deezer.com/search?q=' + recherche + '&output=jsonp',
-            dataType : 'jsonp'
-        });
 
+        const requete = $.ajax({
+            url: 'https://api.deezer.com/search?q=' + recherche + '&output=jsonp&order=' + filter,
+            dataType: 'jsonp'
+        });
         requete.done(traiterResultats);
 
         // use call AJAX
@@ -23,12 +25,25 @@ $(document).ready(function() {
         // add musicItem AJAX
 
         function generateMusicObject(musicItem) {
-            return `<li class="media">
-            <img class="media-object" src="${musicItem.album.cover}">
-            <p class="">${musicItem.title}</p>
-            <p class="">${musicItem.album.title}</p>
-            <p class="">${musicItem.artist.title}</p>
+            return `<li id="${musicItem.id}" class="media">
+            <video controls="" name="media">
+            <source src="${musicItem.preview}" type="audio/mpeg">
+            </video>
+            <img class="media-object albumPicture" src="${musicItem.album.cover}">
+            <div class="infoMusic">
+            <p class="musicTitle">${musicItem.title}</p>
+            <p class="musicDetail">${musicItem.album.title}</p>
+            <p class="musicDetail">${musicItem.artist.name}</p>
+            </div>
+            <button class="addFavorit">Ajouter aux favoris</button>
             </li>`;
         }
     });
+
+    // favorit song management
+    const musicId = $('.media').attr('id');
+    localStorage.content = musicId;
+    $('.media').html(localStorage.content);
+    console.log(musicId)
+
 });
